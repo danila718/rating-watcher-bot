0dab85f98ae1dea349f26dbb605b5371ab6bd6cc
Daniil Simonyan <danila718@gmail.com>
2022-07-11 13:01:23 +0300

    уведомление об отсутствии изменений в течении времени

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ import fs from 'fs';
 import { RatingObject } from "./rating-parser/rating-parser.interface";
 
 export class App {
+    private lastSendTime: number = 0;
+
     constructor(
         private firstSend: boolean,
+        private maxDelayTime: number,
         readonly chatId: string,
         readonly telegram: ITelegram,
         readonly watcher: Watcher,
@@ -18,14 +21,28 @@ export class App {
         event.on('change', async (data: RatingObject, filePath: string) => {
             if (!this.firstSend) {
                 this.firstSend = true;
+                this.lastSendTime = Date.now();
                 return;
             }
             let msg = `<b>Обнаружено изменение в списках!</b>\n${data.lastUpdated}`;
             for (const ratingItem of data.ratingItems) {
                 msg += `\n${ratingItem.directionName}\nНомер в списке: ${ratingItem.position}`;
             }
-            this.logger.info('Sending changes');
-            this.sendDocument(filePath, (data.lastUpdated + '.pdf'), msg);
+            this.logger.info('Sending Changes');
+            await this.sendDocument(filePath, (data.lastUpdated + '.pdf'), msg);
+            this.lastSendTime = Date.now();
+        });
+        event.on('not_change', async (data: RatingObject, filePath: string) => {
+            if (!this.lastSendTime || (Date.now() - this.lastSendTime) < this.maxDelayTime) {
+                return;
+            }
+            let msg = `<b>Изменения в списках не обнаружены в течении ${Math.round(this.maxDelayTime / 3600000 * 100) / 100} часов</b>`;
+            for (const ratingItem of data.ratingItems) {
+                msg += `\n${ratingItem.directionName}\nНомер в списке: ${ratingItem.position}`;
+            }
+            this.logger.info('Sending NOT Change');
+            await this.sendDocument(filePath, (data.lastUpdated + '.pdf'), msg);
+            this.lastSendTime = Date.now();
         });
     }
 
--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ const frequency = process.env.FREQUENCY ?? '60000';
 const directions = process.env.TARGET_DIRECTIONS ? JSON.parse(process.env.TARGET_DIRECTIONS) : undefined;
 const snils = process.env.TARGET_SNILS ?? '123';
 const firstSend = (process.env.FIRST_SEND ?? '0') === '1' ? true : false;
+const maxDelay = process.env.MAX_DELAY ?? '10800000';
 if (!token || !chatId || !url || !fileName) {
     throw new Error('Token, chatId, url, fileName must be specified in .env file');
 }
@@ -29,7 +30,7 @@ const parser: RatingParser = new RatingParser(
     snils, 
 );
 const watcher: Watcher = new Watcher(logger, parser, (__dirname + sep + fileName), url, Number.parseInt(frequency));
-const app = new App(firstSend, chatId, telegram, watcher, logger);
+const app = new App(firstSend,  Number.parseInt(maxDelay), chatId, telegram, watcher, logger);
 
 // starting
 logger.info(`Token: ${token}, ChatId: ${chatId}, fileName: ${fileName}, url: ${url}`);
